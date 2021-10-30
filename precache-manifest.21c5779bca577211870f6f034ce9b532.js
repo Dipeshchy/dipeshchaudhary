b9cfc96341245972ab8578fe0d0b4f54ee1bf9bc
Dipesh Chaudhary <dipeshchaudhary5@gmail.com>
2021-10-30 10:10:55 +0200

    Updates

---
+++ precache-manifest.21c5779bca577211870f6f034ce9b532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73f162bc3285baeb2228c1a623c8147",
+    "url": "/dipeshchy/index.html"
+  },
+  {
+    "revision": "a258bbb08845c30f5399",
+    "url": "/dipeshchy/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "3e7cdf84e018406f03cd",
+    "url": "/dipeshchy/static/js/2.168ca88e.chunk.js"
+  },
+  {
+    "revision": "a258bbb08845c30f5399",
+    "url": "/dipeshchy/static/js/main.d8000fbf.chunk.js"
+  },
+  {
+    "revision": "051219eac021a49c7b33",
+    "url": "/dipeshchy/static/js/runtime~main.83c3ae4d.js"
+  },
+  {
+    "revision": "91503ef13182681f4f75211560c4ac6d",
+    "url": "/dipeshchy/static/media/photo.91503ef1.jpg"
+  }
+]);
